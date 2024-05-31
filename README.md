b4011ccb46809ae82ca3ea9aaa36a174395c58a5
asif1919 <110787824+asif1919@users.noreply.github.com>
2024-05-31 20:53:51 +0600

    fixed all the bugs
    --

---
+++ README.md
@@ -0,0 +1,14 @@
+# Daffodil School PHP & MYSQL
+
+version: 1
+
+## TECHNOLOGIES
+
+1. PHP
+1. MYSQL
+1. BOOTSTRAP 5
+1. HTML
+1. CSS
+1. JS
+
+
---
+++ admin/inc/navbar.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">
+      <img src="../logo.png" width="40">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0"
+          id="navLinks">
+        <li class="nav-item">
+          <a class="nav-link" 
+             aria-current="page" 
+             href="index.php">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="teacher.php">Teachers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="student.php">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="grade.php">Batch</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="section.php">Section</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="class.php">Class</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="registrar-office.php">Registrar-Office</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="course.php">Course</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="message.php">Message</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="settings.php">Settings</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav me-right mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="../logout.php">Logout</a>
+        </li>
+      </ul>
+  </div>
+    </div>
+</nav>
